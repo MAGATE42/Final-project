b6262292be910a21a6783b79e49140be4d2a752a
Harinala <arnalarab@gmail.com>
2025-07-14 10:23:58 +0300

    insert categorie objet

--- v1/sql/insertion.sql
+++ v1/sql/insertion.sql
@@ -3,3 +3,8 @@ INSERT INTO membre (nom, date_naissance, genre, email, ville, mdp, image_profil)
 ('Bob', '1985-05-12', 'Homme', 'bob@example.com', 'Fianarantsoa', 'pass123', 'bob.jpg'),
 ('Charlie', '1992-09-20', 'Homme', 'charlie@example.com', 'Toamasina', 'pass123', 'charlie.jpg'),
 ('Dina', '1995-12-15', 'Femme', 'dina@example.com', 'Mahajanga', 'pass123', 'dina.jpg');
+INSERT INTO categorie_objet (nom_categorie) VALUES
+('Esthétique'),
+('Bricolage'),
+('Mécanique'),
+('Cuisine');
