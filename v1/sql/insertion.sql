678c239c653f889a25556bb522d5a614783bb72f
Harinala <arnalarab@gmail.com>
2025-07-14 10:21:24 +0300

    insertion membres

---
+++ v1/sql/insertion.sql
@@ -0,0 +1,5 @@
+INSERT INTO membre (nom, date_naissance, genre, email, ville, mdp, image_profil) VALUES
+('Alice', '1990-01-01', 'Femme', 'alice@example.com', 'Antananarivo', 'pass123', 'alice.jpg'),
+('Bob', '1985-05-12', 'Homme', 'bob@example.com', 'Fianarantsoa', 'pass123', 'bob.jpg'),
+('Charlie', '1992-09-20', 'Homme', 'charlie@example.com', 'Toamasina', 'pass123', 'charlie.jpg'),
+('Dina', '1995-12-15', 'Femme', 'dina@example.com', 'Mahajanga', 'pass123', 'dina.jpg');
