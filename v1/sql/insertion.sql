c0432acc0350394657487c555fd8739dde5d752e
Harinala <arnalarab@gmail.com>
2025-07-14 10:58:06 +0300

    modif insertion

--- v1/sql/insertion.sql
+++ v1/sql/insertion.sql
@@ -1,8 +1,8 @@
 INSERT INTO membre (nom, date_naissance, genre, email, ville, mdp, image_profil) VALUES
-('Alice', '1990-01-01', 'Femme', 'alice@example.com', 'Antananarivo', 'pass123', 'alice.jpg'),
-('Bob', '1985-05-12', 'Homme', 'bob@example.com', 'Fianarantsoa', 'pass123', 'bob.jpg'),
-('Charlie', '1992-09-20', 'Homme', 'charlie@example.com', 'Toamasina', 'pass123', 'charlie.jpg'),
-('Dina', '1995-12-15', 'Femme', 'dina@example.com', 'Mahajanga', 'pass123', 'dina.jpg');
+('Harinala', '2007-07-04', 'Homme', 'harinala@gmail.com', 'Antananarivo', 'mdp1', 'harinala.jpg'),
+('Rajo', '1985-05-12', 'Homme', 'rajo@gmail.com', 'Fianarantsoa', 'mdp2', 'rajo.jpg'),
+('Charlie', '1992-09-20', 'Homme', 'charlie@gmail.com', 'Toamasina', 'mdp3', 'charlie.jpg'),
+('Dina', '1995-12-15', 'Femme', 'dina@gmail.com', 'Mahajanga', 'mdp4', 'dina.jpg');
 
 INSERT INTO categorie_objet (nom_categorie) VALUES
 ('Esthétique'),
