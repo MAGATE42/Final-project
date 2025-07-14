4478b71747fbe0ac19f6b1f1cec9ada7c683b06d
Harinala <arnalarab@gmail.com>
2025-07-14 11:31:58 +0300

    objet part2

--- v1/page/objets.php
+++ v1/page/objets.php
@@ -11,3 +11,34 @@ if (isset($_GET["categorie"])) {
 $categories = getCategories($connexion);
 $objets = getObjets($connexion, $categorie_id);
 ?>
+<div class="container mt-4">
+    <h4>Liste des objets</h4>
+
+    <form method="get" action="model.php">
+    <input type="hidden" name="p" value="objets">
+    
+    <div class="row">
+        <div class="col-md-6">
+     <select name="categorie" class="form-control">
+                <option value="">-- Toutes les catégories --</option>
+                <?php
+          foreach ($categories as $cat) {
+                $selected = "";
+
+              if ($categorie_id == $cat["id_categorie"]) {
+                        $selected = "selected";
+                    }
+            ?>
+                <option value="<?= $cat["id_categorie"] ?>" <?= $selected ?>>
+                        <?= $cat["nom_categorie"] ?>
+                </option>
+            <?php
+                }
+                ?>
+    </select>
+        </div>
+        <div class="col-md-2">
+            <input type="submit" value="Filtrer" class="btn btn-primary">
+        </div>
+    </div>
+    </form>
